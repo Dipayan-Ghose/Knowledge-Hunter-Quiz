92312efe41a5e7a6129f92b9a343f6a5a7b03023
Dipayan <ghosedipayen@gmail.com>
2022-10-21 13:59:44 +0600

    rechartadded

--- src/components/Statistics/Chart.js
+++ src/components/Statistics/Chart.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
 
 const Chart = () => {
-    return (
-        <div>
-            this is statistics
-        </div>
-    );
-};
 
+  const charts =useLoaderData();
+console.log(charts);
+  return (
+    <ResponsiveContainer width="100%" height={450}>
+     <AreaChart
+          width={600}
+          height={400}
+          data={charts.data}
+          margin={{
+            top: 50,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Area type="monotone" dataKey="total" stroke="#8884d8" fill="#8884d8" />
+        </AreaChart>
+       </ResponsiveContainer>
+
+    );
+    };
 export default Chart;
+
+
+
+
+
